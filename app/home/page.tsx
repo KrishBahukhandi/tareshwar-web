7c065c21ef8e0c00004fc828822d16d987d458df
Krish Bahukhandi <krishbahukhandi35@gmail.com>
2026-04-12 16:30:31 +0530

    Handle course fetch failure on home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,14 +1,12 @@
 import { HomePage } from "@/components/layout/home-page";
 import { getCourses, STATIC_COURSES } from "@/lib/courses";
-import { getTeachers, STATIC_TEACHERS } from "@/lib/teachers";
 
 export default async function HomeRoutePage() {
-  const [courses, teachers] = await Promise.all([getCourses(), getTeachers()]);
+  const courses = await getCourses().catch(() => []);
 
   return (
     <HomePage
       courses={courses.length ? courses : STATIC_COURSES}
-      teachers={teachers.length ? teachers : STATIC_TEACHERS}
     />
   );
 }
